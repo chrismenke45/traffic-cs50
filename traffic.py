68189010654c5c2dc0b9ec970889c00304ff9728
chrismenke45 <chrismenke45@gmail.com>
2024-02-27 17:45:30 -0800

    switched back to full data set

--- traffic.py
+++ traffic.py
@@ -9,8 +9,8 @@ from sklearn.model_selection import train_test_split
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
-#NUM_CATEGORIES = 43
-NUM_CATEGORIES = 3
+NUM_CATEGORIES = 43
+#NUM_CATEGORIES = 3
 TEST_SIZE = 0.4
 
 
